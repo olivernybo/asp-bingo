c8bff993b9ae4c365dc7d6fb35241231f31e79fc
Oliver Nybo <root@olivernybo.com>
2020-11-25 13:12:44 +0100

    pretty js

--- asp-bingo.Web/wwwroot/js/chat.js
+++ asp-bingo.Web/wwwroot/js/chat.js
@@ -1,27 +1,23 @@
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+const connection = new signalR.HubConnectionBuilder().withUrl('/chatHub').build()
 
 //Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
+document.getElementById('sendButton').disabled = true
 
-connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+connection.on('ReceiveMessage', (user, message) => {
+    const msg = message.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+    const encodedMsg = user + ' says ' + msg
+    const li = document.createElement('li')
+    li.textContent = encodedMsg
+    document.getElementById('messagesList').appendChild(li)
 });
 
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+connection.start()
+	.then(() => document.getElementById('sendButton').disabled = false)
+	.catch(err => console.error(err.toString()))
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
-});
+document.getElementById('sendButton').addEventListener('click', event => {
+    const user = document.getElementById('userInput').value
+    const message = document.getElementById('messageInput').value
+    connection.invoke('SendMessage', user, message).catch(err => console.error(err.toString()))
+    event.preventDefault()
+})
