733459a6c2b83dbdf5cee756e3d2c5c14e66fc6a
Oliver Nybo <root@olivernybo.com>
2020-11-26 23:34:21 +0100

    starting small refactor of generate sheet

--- asp-bingo.Web/Services/BingoService.cs
+++ asp-bingo.Web/Services/BingoService.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -81,6 +82,18 @@ namespace asp_bingo.Web.Services
                     || (number < 80 && c7++ < 3)
                     || (number <= 90 && c8++ < 3))) sheet.Add(number);
             }
+
+            List<int> row0 = new List<int>(sheet.Take(5));
+            List<int> row1 = new List<int>(sheet.Skip(5).Take(5));
+            List<int> row2 = new List<int>(sheet.Skip(10).Take(5));
+
+            row0.Sort();
+            row1.Sort();
+            row2.Sort();
+
+            // todo place 0's
+
+            sheet = row0.Concat(row1).Concat(row2).ToList();
 
             return sheet.ToArray();
         }
