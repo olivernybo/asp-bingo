0cca14e8067cf4f47ef4a2544a479ad9bf8d0a58
Oliver Nybo <root@olivernybo.com>
2020-11-27 16:59:44 +0100

    bingo marking

--- asp-bingo.Web/wwwroot/js/bingo.js
+++ asp-bingo.Web/wwwroot/js/bingo.js
@@ -6,9 +6,14 @@ bingoCconnection.on('BingoCallerRecieve', number => {
 
 bingoCconnection.on('Sheet', sheet => {
 	const tds = document.querySelectorAll('table .bingoCol')
-	for (let i = 0; i < tds.length; i++)
+	for (let i = 0; i < tds.length; i++) {
+		tds[i].addEventListener('click', () => {
+			if (tds[i].classList.contains('bingoMarked')) tds[i].classList.remove('bingoMarked')
+			else tds[i].classList.add('bingoMarked')
+		})
 		if (sheet[i])
 			tds[i].innerText = sheet[i]
+	}
 })
 
 bingoCconnection.start()
