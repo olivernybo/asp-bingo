7276754d6a27bdcb4fc962f32f3f68f4fde8f606
Oliver Nybo <root@olivernybo.com>
2020-11-27 15:54:00 +0100

    a bit of styling

--- asp-bingo.Web/wwwroot/js/bingo.js
+++ asp-bingo.Web/wwwroot/js/bingo.js
@@ -5,10 +5,10 @@ bingoCconnection.on('BingoCallerRecieve', number => {
 })
 
 bingoCconnection.on('Sheet', sheet => {
-	const tds = document.querySelectorAll('table .col')
-	for (let i = 0; i < tds.length; i++) {
-		tds[i].innerText = sheet[i]
-	}
+	const tds = document.querySelectorAll('table .bingoCol')
+	for (let i = 0; i < tds.length; i++)
+		if (sheet[i])
+			tds[i].innerText = sheet[i]
 })
 
 bingoCconnection.start()
